3b6ad9591f02d487a2e37df263727aec22c26d37
NilKel <kelkar.neel@gmail.com>
2016-09-19 23:09:10 +0530

    I'll replace the buttons with fancy stuff that I'll make later using photoshop or something

--- app/src/main/java/com/example/neel/bookingapp/MainActivity.java
+++ app/src/main/java/com/example/neel/bookingapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.neel.bookingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openfooty(View v){
+        startActivity(new Intent(MainActivity.this, Football.class));
+    }
+
+    public void badmint(View v){
+        startActivity(new Intent(MainActivity.this, Badminton.class));
+    }
+
+    public void titty(View v){
+        startActivity(new Intent(MainActivity.this, TableTennis.class));
     }
 }
