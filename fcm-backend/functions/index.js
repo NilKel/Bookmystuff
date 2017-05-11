a070b9bc44b370bb60aaadc071d21326ef623110
Sushrut Shringarputale <sps5394@psu.edu>
2017-05-11 08:06:29 +0000

    Finished function creation

--- fcm-backend/functions/index.js
+++ fcm-backend/functions/index.js
@@ -1,4 +1,13 @@
 var functions = require('firebase-functions');
+var admin = require("firebase-admin");
+var serviceAccount = require('./bookmystuff-79c2e-firebase-adminsdk-4wglh-9054d93f93.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bookmystuff-79c2e.firebaseio.com"
+});
+
+// functions.on
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +15,14 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.sendMessage = functions.database.ref('/lobbies/{lobby_id}/messages')
+                            .onWrite(event => {
+                               const original = event.data.val();
+                               console.log(event.params.lobby_id);
+                               var topic = event.params.lobby_id;
+                               var payload = {
+                                   data: original
+                               };
+                               return admin.messaging().sendToTopic(topic, payload);
+                            });
